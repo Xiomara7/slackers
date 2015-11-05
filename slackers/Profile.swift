b30d273c6919d1a6ac3a572dc298b161e5ad11d3
Xiomara Figueroa <figueroaxiomara7@gmail.com>
2015-11-04 23:09:21 -0400

    Initialized classes for entities

---
+++ slackers/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  slackers
+//
+//  Created by Xiomara on 11/4/15.
+//  Copyright © 2015 Xiomara. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Profile)
+class Profile: NSManagedObject {
+    @NSManaged var email: String?
+    @NSManaged var phone: String?
+    @NSManaged var skype: String?
+    
+    @NSManaged var image_24: String?
+    @NSManaged var image_32: String?
+    @NSManaged var image_48: String?
+    @NSManaged var image_72: String?
+    @NSManaged var image_192: String?
+    
+    @NSManaged var user: User?
+    
+}
+
---
+++ slackers/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  slackers
+//
+//  Created by Xiomara on 11/4/15.
+//  Copyright © 2015 Xiomara. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+class User: NSManagedObject {
+    @NSManaged var is_admin: NSNumber?
+    @NSManaged var theme_color: String?
+    @NSManaged var user_id: String?
+    @NSManaged var username: String?
+    @NSManaged var real_name: String?
+    @NSManaged var profile: Profile?
+}
